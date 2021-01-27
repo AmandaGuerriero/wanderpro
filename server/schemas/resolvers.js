5a81a2e513afc947502a539c8a75223fdc9d2717
Amanda Guerriero <ag.codingclass@gmail.com>
2021-01-26 21:00:37 -0800

    Itinerary typeDefs & resolvers added
    typeDefs for Itinerary, Queries for all itineraries and a single itinerary by ID, Mutations for adding an itinerary and updating an itinerary.
    
    resolvers for the typeDefs created

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,17 @@
+const { Itinerary } = require('../models');
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
+    itineraries: async () => {
+      return await Itinerary.find();
+    },
+    itineraryById: async (parent, { _id }) => {
+      return await Itinerary.findById(_id)
+    }
+  },
+  Mutation: {
+    addItinerary: async (parent, args) => {
+      const itinerary = await Itinerary.create(args);
+      return itinerary;
     }
   }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,24 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Query {
-  helloWorld: String
+type Itinerary {
+  _id: String
+  title: String
+  description: String
+  dateBegin: String
+  dateEnd: String
 }
+
+type Query {
+  itineraries(title: String): [Itinerary]
+  itineraryById(_id: ID!): Itinerary
+}
+
+type Mutation {
+  addItinerary(title: String!, description: String, dateBegin: String, dateEnd: String): Itinerary
+  updateItinerary(_id: ID!, title: String!, description: String, dateBegin: String, dateEnd: String): Itinerary
+}
+
 `;
 
 module.exports = typeDefs;
