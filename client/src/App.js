d597ce3eb7c65cd00097b5ee65a5b795c46233ca
Symphony <55611595+lnance6@users.noreply.github.com>
2021-01-31 16:52:12 -0800

    Implemented Donate Button
    Created Donate button that connects to Stripe and allows users to input credit/debit card information.  It can accessed at '/donate'.  You have to use https protocol in order for this to work which you can do locally by installing the ngrok service (https://ngrok.com/)

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -7,6 +9,7 @@ import { StoreProvider } from './utils/GlobalState';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Donate from "./pages/Donate";
 import Nav from "./components/Nav";
 import CreateItinerary from "./components/CreateItinerary/CreateItinerary"
 import Create from './components/Create-activity'
@@ -24,6 +27,8 @@ const client = new ApolloClient({
   uri: '/graphql',
 })
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 function App() {
   const [ latitude, setLatitude ] = useState('')
   const [ longitude, setLongitude] = useState('')
@@ -33,26 +38,31 @@ function App() {
 
     <ApolloProvider client={client}>
       <Router>
+      <Elements stripe={stripePromise}>
       <StoreProvider>
         <div>
           <StoreProvider>
               <Nav />
               <Switch>
-                <Route path="/create">
-                  <CreateItinerary setLatitude={setLatitude} setLongitude={setLongitude} />
-                </Route>
+              
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
+                <Route exact path="/donate" component={Donate} />
+                <Route path="/create">
+                  <CreateItinerary setLatitude={setLatitude} setLongitude={setLongitude} />
+                </Route>
                 <Route exact path="/activity" component={Create} />
                 <Route path="/summary">
                   <Summary latitude={latitude} longitude={longitude}/>
                 </Route>
+               
               </Switch>
             </StoreProvider>
 
         </div>
       </StoreProvider>
+      </Elements>
       </Router>
     </ApolloProvider>
   );
