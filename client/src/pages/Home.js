ff0ac5f4ffd7a30d52a080aeee0abcc632eaa9f7
ASC8 <asamcent@gmail.com>
2021-02-03 03:22:03 -0800

    Styling for Nav and HomePage Jumbo Add Jumbo image for homepage, with linear gradient. Added border bottom to Nav because homepage image and Nav cloud background were conflicting an the line helped make it seem as though the clouds were connecting to one another versus being two different images.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ import { QUERY_ACTIVITIES } from '../utils/queries';
 import { useQuery } from '@apollo/react-hooks';
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ACTIVITIES);
@@ -10,7 +12,21 @@ const Home = () => {
 
   return (
       <div>
-        <h1>HOME</h1>
+        {/* <h1>HOME</h1> */}
+        <div className="homepage-jumbo">
+        {
+        Auth.loggedIn() ?
+        <Link to="/profile">
+        <button>
+        Go to Profile
+        </button>
+        </Link>
+          :
+          <button href="/signup" className="homepage-signup">
+        <FontAwesomeIcon icon={faUserPlus}/> Create an account 
+        </button>
+       }
+        </div>
         {activities && 
           activities.map(activity => (
             <div key={activity._id} className="card-box">
@@ -26,18 +42,7 @@ const Home = () => {
                   </div>
           </div>
       ))}
-      {
-      Auth.loggedIn() ?
-      <Link to="/profile">
-      <button>
-      Go to Profile
-      </button>
-      </Link>
-        :
-        <button href="/signup">
-      Create an account
-      </button>
-    }
+      
     </div>
   )
 };
