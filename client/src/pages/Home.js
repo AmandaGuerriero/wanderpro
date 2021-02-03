1fb3441af322d0ecf96535d5f432a948874c6acc
Amanda Guerriero <ag.codingclass@gmail.com>
2021-02-02 22:22:22 -0800

    added a console.log for activities to homepage to troubleshoot heroku

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ACTIVITIES);
   const activities = data?.activities || [];
-
+  console.log(activities)
   return (
       <div>
         <h1>HOME</h1>
